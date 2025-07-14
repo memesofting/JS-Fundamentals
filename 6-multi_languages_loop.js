93eba8ed45b4f1cb34c02ac11f2608df36529543
memesofting <bibitayoadekunle00@gmail.com>
2025-07-14 11:29:37 +0000

    correcting 6

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,12 @@
-const message_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+/*const message_array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for(let i = 0; i < message_array.length; i++)
 {
 	console.log(message_array[i]);
+}*/
+
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < languages.length; i++) {
+	  console.log(languages[i]);
 }
