3f56f89a185d99b18d768599fd73e3e372449691
memesofting <bibitayoadekunle00@gmail.com>
2025-07-15 21:24:46 +0100

    print multiple times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,13 +1,23 @@
 const arg = Number(process.argv[2]);
 
+// if(isNaN(arg) || arg < 0 || !Number.isInteger(arg))
+// {
+// 	console.log("Missing number of occurrences");
+// }
+// else
+// {
+//     for(let i = 0; i < arg; i++)
+//     {
+//         console.log("C is fun");
+//     }
+// }
+
 if(isNaN(arg) || arg < 0 || !Number.isInteger(arg))
 {
-	console.log("Missing number of occurrences");
+    console.log("Missing number of occurrences");
+    return;
 }
-else
+for(let i = 0; i < arg; i++)
 {
-    for(let i = 0; i < arg; i++)
-    {
-        console.log("C is fun");
-    }
+    console.log("C is fun");
 }
