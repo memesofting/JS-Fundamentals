b19dfbe9f12d6c99007a74dc3530534f8354f525
memesofting <bibitayoadekunle00@gmail.com>
2025-07-15 22:00:21 +0100

    refactor braces

--- 8-square.js
+++ 8-square.js
@@ -1,15 +1,12 @@
 const size = Number(process.argv[2]);
 
-if(isNaN(size))
-{
+if(isNaN(size)){
 	console.log("Missing size");
 }
 else
 {
-    for(let i = 0; i < size; i++)
-    {
-        for(let j = 0; j < size; j++)
-        {
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
             process.stdout.write('X');
         }
         console.log();
